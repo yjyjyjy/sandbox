f4a9c60e134c6b582b90a0d5c5e62d8933f591df
Junyu Yang <prowessyang@gmail.com>
2021-11-26 00:49:18 -0500

    frontend, backend deployed

--- waves/waveportal-starter-project-main/src/App.js
+++ waves/waveportal-starter-project-main/src/App.js
@@ -1,29 +1,45 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import './App.css';
 
-export default function App() {
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
 
-  const wave = () => {
-    
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
   }
-  
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
 }
+
+export default App
